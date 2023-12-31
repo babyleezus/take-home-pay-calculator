f4f0e95b976dfe051ab465a05249d55460e1e541
Baby Leezus <118462761+babyleezus@users.noreply.github.com>
2023-12-31 19:02:36 +0000

    cleaned up js and added radio buttons

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
-/*PESUDOCODE 
--Get the value in the income input field
--take this value and figures out which function needs to be applied on it
--code a function for calculating the national insurance - EXTRA
--code function to break down salary into take home pay and tax by week month and year - EXTRA, grid?
-expand the container so that the result is displayed
-create an event listener for the submit button so that all these functions are triggered when it is pressed
-take this value and add it to the html 
-*/
-
-
-// var timePeriod = document.getElementsByName("time-period")
-
-
-// var pensionOption = document.getElementsByName("pension-option")
-
-// var pensionContribution = document.getElementById("pension-contribution")
-
-// var studentLoan = document.getElementsByName("student-loan-option")
-
-// var studentLoanContribution = document.getElementById("student-loan-contribution")
+const submit = document.getElementById('submit-btn')
 var income = document.getElementById("income")
 
 var income;
 var takeHomePay;
+var NIContribution;
 
+
+    
 function noIncomeTax (){
     if(income <= 12570);{
     console.log("You have no income tax to pay")}
 }
-// noIncomeTax();
-
-// income = 30000
 
 function lowerRateIncomeTax(){
     let taxableIncome = income - 12751;
@@ -39,10 +19,6 @@ function lowerRateIncomeTax(){
     console.log("Your total income tax to pay is £ " + totalTax);
     console.log("Your total take home pay is £ " + takeHomePay);
 }
-
-// lowerRateIncomeTax()
-
- income = 51000;
 
 function higherRateIncomeTax(){
     var lowerTax = (50270 - 12571)* 0.2;
@@ -53,8 +29,6 @@ function higherRateIncomeTax(){
     console.log("Your total take home pay is £ " + takeHomePay)
 }
 
-higherRateIncomeTax()
-
 function highestRateIncomeTax(){
     var lowerTax = (50270 - 12571)* 0.2;
     var higherTax = (125140 - 50271)* 0.4;
@@ -64,6 +38,29 @@ function highestRateIncomeTax(){
     let takeHomePay = income - totalTax;
     console.log("Your total take home pay is £ " + takeHomePay)
 }
+
+switch (income){
+    case income <= 12570:
+    noIncomeTax()
+    break;
+    case income >= 12570 && < 50270:
+    lowerRateIncomeTax()
+    break;
+    case income >= 50271 && < 125140:
+    higherRateIncomeTax()
+    break;
+    case income >= 125141:
+    highestRateIncomeTax
+    break;
+}
+
+
+
+
+
+
+
+
 
 //  highestRateIncomeTax()
 
